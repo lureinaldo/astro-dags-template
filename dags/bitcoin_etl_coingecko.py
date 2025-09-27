4226c5c0c72318232b6cf85b204291842d957086
lureinaldo <luciana.atuaria@gmail.com>
2025-09-27 13:42:51 -0300

    Update bitcoin_etl_coingecko.py

--- dags/bitcoin_etl_coingecko.py
+++ dags/bitcoin_etl_coingecko.py
@@ -82,7 +82,7 @@ def fetch_bitcoin_history_from_coingecko():
 @dag(
     default_args=DEFAULT_ARGS,
     schedule="0 0 * * *",  # diário à 00:00 UTC
-    start_date=pendulum.datetime(2025, 9, 19, tz="UTC"),
+    start_date=pendulum.datetime(2025, 9, 1, tz="UTC"),
     catchup=True,
     owner_links={
         "Alex Lopes": "mailto:alexlopespereira@gmail.com",
